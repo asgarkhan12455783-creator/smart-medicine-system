0881be5efdd2ff7ef89336ef742d514fe7b7d4ec
asgarkhan12455783-creator <asgarkhan12455783@gmail.com>
2026-08-15 18:42:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,30 +1,51 @@
-// Firebase App
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
-// Firebase Firestore
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
-// Firebase Authentication
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
-// Your Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBOXAYel4xvnXSv31GGhhy4bzSbPLgblNw",
-  authDomain: "smart-medicine-system-d8b6c.firebaseapp.com",
-  projectId: "smart-medicine-system-d8b6c",
-  storageBucket: "smart-medicine-system-d8b6c.firebasestorage.app",
-  messagingSenderId: "257438247903",
-  appId: "1:257438247903:web:dae15d7f02339e3e0fb26a"
+
+    apiKey: "AIzaSyBOXAYel4xvnXSv31GGhhy4bzSbPLgblNw",
+
+    authDomain:
+        "smart-medicine-system-d8b6c.firebaseapp.com",
+
+    projectId:
+        "smart-medicine-system-d8b6c",
+
+    storageBucket:
+        "smart-medicine-system-d8b6c.firebasestorage.app",
+
+    messagingSenderId:
+        "257438247903",
+
+    appId:
+        "1:257438247903:web:dae15d7f02339e3e0fb26a"
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore database "aban"
-const db = getFirestore(app, "aban");
+const app =
+    initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication
-const auth = getAuth(app);
 
-// Export Firestore and Authentication
-export { db, auth };
+const db =
+    getFirestore(app, "aban");
+
+
+const auth =
+    getAuth(app);
+
+
+export {
+    db,
+    auth
+};
